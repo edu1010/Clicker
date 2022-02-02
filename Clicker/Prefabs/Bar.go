fd7488e25bba114a057eb8a08b483ce8796d60c8
edu1010 <39130123+edu1010@users.noreply.github.com>
2022-02-02 08:20:48 +0100

    CreateBar

--- Clicker/Prefabs/Bar.go
+++ Clicker/Prefabs/Bar.go
@@ -1,3 +1,18 @@
+components {
+  id: "Bar"
+  component: "/Scripts/Bar.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
